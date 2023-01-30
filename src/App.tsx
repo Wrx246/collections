529740b5bc7c0898b0edd8c929571807fc2ebc29
Maxim Kuzmianok <jst.wrx@gmail.com>
2023-01-30 21:17:59 +0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login';
+import Main from './pages/Main';
+import ProtectedRoutes from './pages/ProtectedRoutes';
+import Registration from './pages/Registration';
 
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/" element={<Main />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/ProtectedRoutes.tsx
@@ -0,0 +1,11 @@
+import {Outlet, Navigate} from 'react-router-dom';
+
+const ProtectedRoutes = () => {
+    const isAuth = true;
+
+    return (
+      isAuth ? <Outlet /> : <Navigate to='/login' />
+    );
+}
+
+export default ProtectedRoutes
